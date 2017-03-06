09af5608236e0ba031deac7066148fef0478f905
trevorfiez <fieztr@onid.oregonstate.edu>
2017-03-05 16:01:00 -0800

    Added evaluation stuff for cifar-10 softmax

--- conformal/cifar10/softmax/cifar10_eval.py
+++ conformal/cifar10/softmax/cifar10_eval.py
@@ -112,6 +112,52 @@ def eval_once(saver, summary_writer, top_k_op, summary_op):
     coord.request_stop()
     coord.join(threads, stop_grace_period_secs=10)
 
+def conformal_eval(saver, summary_writer, summary_op, logits, labels):
+	with tf.Session() as sess:
+		ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+		if ckpt and ckpt.model_checkpoint_path:
+			# Restores from checkpoint
+			saver.restore(sess, ckpt.model_checkpoint_path)
+			# Assuming model_checkpoint_path looks something like:
+			#   /my-favorite-path/cifar10_train/model.ckpt-0,
+			# extract global_step from it.
+			global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+		coord = tf.train.Coordinator()		
+		for i in range(1, 11):
+			top_k_op = tf.nn.in_top_k(logits, labels, i)
+
+			
+			try:
+				threads = []
+				for qr in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
+					threads.extend(qr.create_threads(sess, coord=coord, daemon=True,
+								 start=True))
+
+				num_iter = int(math.ceil(FLAGS.num_examples / FLAGS.batch_size))
+				true_count = 0  # Counts the number of correct predictions.
+				total_sample_count = num_iter * FLAGS.batch_size
+				step = 0
+				while step < num_iter and not coord.should_stop():
+					predictions = sess.run([top_k_op])
+					true_count += np.sum(predictions)
+					step += 1
+
+			
+				recall = true_count / total_sample_count
+				precision = true_count / (total_sample_count * i)
+				print('Recall @ %d = %.3f' % (i, recall))
+				print('Precision @ %d = %.3f' % (i, precision))
+
+				summary = tf.Summary()
+				summary.ParseFromString(sess.run(summary_op))
+				summary.value.add(tag='Recall @ %d' % (i), simple_value=recall)
+				summary.value.add(tag='Precision @ %d' % (i), simple_value=precision) 
+				summary_writer.add_summary(summary, global_step)
+			except Exception as e:  # pylint: disable=broad-except
+				coord.request_stop(e)
+		coord.request_stop()
+		coord.join(threads, stop_grace_period_secs=10)
+
 
 def evaluate():
   """Eval CIFAR-10 for a number of steps."""
@@ -139,10 +185,11 @@ def evaluate():
     summary_writer = tf.summary.FileWriter(FLAGS.eval_dir, g)
 
     while True:
-      eval_once(saver, summary_writer, top_k_op, summary_op)
-      if FLAGS.run_once:
-        break
-      time.sleep(FLAGS.eval_interval_secs)
+      conformal_eval(saver, summary_writer, summary_op, logits, labels)
+      #eval_once(saver, summary_writer, top_k_op, summary_op)
+      #if FLAGS.run_once:
+      #  break
+      #time.sleep(FLAGS.eval_interval_secs)
 
 
 def main(argv=None):  # pylint: disable=unused-argument
